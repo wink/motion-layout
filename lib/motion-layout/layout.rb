8fc8799de8114aad951133c2ef88c0cf663c5b3f
Micah Winkelspecht <winkelspecht@gmail.com>
2013-07-21 21:19:56 -0700

    Adding ability to use any combination of options for each line of VFL and removing default options that use centering.

--- lib/motion-layout/layout.rb
+++ lib/motion-layout/layout.rb
@@ -1,5 +1,22 @@
 module Motion
   class Layout
+    LAYOUT_FORMAT_OPTIONS = {
+      left: NSLayoutFormatAlignAllLeft,
+      right: NSLayoutFormatAlignAllRight,
+      top: NSLayoutFormatAlignAllTop,
+      bottom: NSLayoutFormatAlignAllBottom,
+      leading: NSLayoutFormatAlignAllLeading,
+      trailing: NSLayoutFormatAlignAllTrailing,
+      center_x: NSLayoutFormatAlignAllCenterX,
+      center_y: NSLayoutFormatAlignAllCenterY,
+      baseline: NSLayoutFormatAlignAllBaseline,
+      mask: NSLayoutFormatAlignmentMask,
+      leading_to_trailing: NSLayoutFormatDirectionLeadingToTrailing,
+      left_to_right: NSLayoutFormatDirectionLeftToRight,
+      right_to_left: NSLayoutFormatDirectionRightToLeft,
+      direction_mask: NSLayoutFormatDirectionMask
+    }
+    
     def initialize(&block)
       @verticals   = []
       @horizontals = []
@@ -21,12 +38,16 @@ module Motion
       @view = view
     end
 
-    def horizontal(horizontal, options = 0)
-      @horizontals << [horizontal, options]
+    def horizontal(horizontal, *options)
+      @horizontals << [horizontal, options_bitmask(*options)]
     end
 
-    def vertical(vertical, options = 0)
-      @verticals << [vertical, options]
+    def vertical(vertical, *options)
+      @verticals << [vertical, options_bitmask(*options)]
+    end
+    
+    def options_bitmask(*options)
+      options.map { |o| o.is_a?(Integer) ? o : LAYOUT_FORMAT_OPTIONS[o] || 0 }.inject(:|)
     end
 
     private
